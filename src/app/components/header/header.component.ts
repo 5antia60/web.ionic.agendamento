3254e5f622e4aa74a55aed7d49c1a2ae33ab548a
Santiago <sammysan007@gmail.com>
2022-11-27 13:18:16 -0300

    feature(tabsPage): added page layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+//#region Imports
+
+import { Component } from '@angular/core';
+
+//#endregion
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderComponent {}
