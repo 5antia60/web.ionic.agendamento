c696e94f7645b078d2f51d4aa9ecd8eaf991e19f
Santiago <sammysan007@gmail.com>
2022-11-27 12:58:29 -0300

    feature(header): added new component, pages

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -2,18 +2,18 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { TabsPageRoutingModule } from './tabs-routing.module';
-
+import { HeaderModule } from '../../components/header/header.module';
+import { TabsRoutingModule } from './tabs-routing.module';
 import { TabsPage } from './tabs.page';
 
 @NgModule({
   imports: [
-    IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    IonicModule,
+    TabsRoutingModule,
+    HeaderModule,
   ],
-  declarations: [TabsPage]
+  declarations: [TabsPage],
 })
 export class TabsPageModule {}
