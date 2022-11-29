15ff6921d4f6d32edfaaba8b179526519091c7f4
Santiago <sammysan007@gmail.com>
2022-11-29 00:12:15 -0300

    refactoro(environment.prod)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDAbTYRsV1g20SoHGDs31v-ZUD2JQim-do',
+    authDomain: 'agendamento-de-reuniao-app.firebaseapp.com',
+    projectId: 'agendamento-de-reuniao-app',
+    storageBucket: 'agendamento-de-reuniao-app.appspot.com',
+    messagingSenderId: '543458276382',
+    appId: '1:543458276382:web:5bea9e99e1400209216a0f',
+    measurementId: 'G-DE9PYP2NPZ',
+  },
 };
